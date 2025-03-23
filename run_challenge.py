3c0a1e443fb91cc655b67b20fa68d29f54f92c31
Thalyson <thalyon.silva@ifce.edu.br>
2025-03-23 16:24:41 -0300

    Remove timeout command and set shell as true to compile program

--- run_challenge.py
+++ run_challenge.py
@@ -7,18 +7,33 @@ import platform
 CPLEX_PATH = "$HOME/CPLEX_Studio2211/opl/bin/arm64_osx/"
 OR_TOOLS_PATH = "$HOME/Documents/or-tools/build/lib/"
 
-USE_CPLEX = True
-USE_OR_TOOLS = True
+USE_CPLEX = False
+USE_OR_TOOLS = False
 
-MAX_RUNNING_TIME = "605s"
+MAX_RUNNING_TIME = "599s"
+
 
 def compile_code(source_folder):
     print(f"Compiling code in {source_folder}...")
     # Change to the source folder
     os.chdir(source_folder)
 
+    print(f"Directory changed to {os.getcwd()}")
+
     # Run Maven compile
-    result = subprocess.run(["mvn", "clean", "package"], capture_output=True, text=True)
+    result = None
+
+    try:
+        result = subprocess.run(
+            ["mvn", "clean", "package"],
+            capture_output=True,
+            text=True,
+            shell=True,  # TODO: Change for linux
+        )
+    except Exception as e:
+        print("mvn Error.")
+        print(e)
+        return False
 
     if result.returncode != 0:
         print("Maven compilation failed:")
@@ -53,12 +68,21 @@ def run_benchmark(source_folder, input_folder, output_folder):
         if filename.endswith(".txt"):
             print(f"Running {filename}")
             input_file = os.path.join(input_folder, filename)
-            output_file = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}.txt")
+            output_file = os.path.join(
+                output_folder, f"{os.path.splitext(filename)[0]}.txt"
+            )
             with open(output_file, "w") as out:
                 # Main Java command
-                cmd = [timeout_command, MAX_RUNNING_TIME, "java", "-Xmx16g", "-jar", "target/ChallengeSBPO2025-1.0.jar",
-                       input_file,
-                       output_file]
+                cmd = [
+                    # timeout_command, # TODO: Change for linux
+                    # MAX_RUNNING_TIME, # TODO: Change for linux
+                    "java",
+                    "-Xmx16g",
+                    "-jar",
+                    "target/ChallengeSBPO2025-1.0.jar",
+                    input_file,
+                    output_file,
+                ]
                 if USE_CPLEX or USE_OR_TOOLS:
                     cmd.insert(3, f"-Djava.library.path={libraries}")
 
@@ -70,7 +94,10 @@ def run_benchmark(source_folder, input_folder, output_folder):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python run_challenge.py <source_folder> <input_folder> <output_folder>")
+        print(sys.argv)
+        print(
+            "Usage: python run_challenge.py <source_folder> <input_folder> <output_folder>"
+        )
         sys.exit(1)
 
     source_folder = sys.argv[1]
